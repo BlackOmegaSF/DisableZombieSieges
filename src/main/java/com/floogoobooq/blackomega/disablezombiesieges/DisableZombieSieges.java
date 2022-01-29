69d6c17b602aeadca54590f345c2f716cf2a1a4c
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-29 09:50:35 -0800

    Catch and cancel siege event

--- src/main/java/com/floogoobooq/blackomega/disablezombiesieges/DisableZombieSieges.java
+++ src/main/java/com/floogoobooq/blackomega/disablezombiesieges/DisableZombieSieges.java
@@ -1,9 +1,31 @@
 package com.floogoobooq.blackomega.disablezombiesieges;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DisableZombieSieges extends JavaPlugin {
+import java.util.logging.Level;
 
+public class DisableZombieSieges extends JavaPlugin implements Listener {
 
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @EventHandler
+    public void spawnFromSiege(CreatureSpawnEvent event) {
+        if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.VILLAGE_INVASION) { //If mob is spawned due to invasion
+            event.setCancelled(true);
+            //Log cancellation event to server console
+            getLogger().log(Level.INFO, "Prevented invasion entity from spawning");
+        }
+    }
 
 }
